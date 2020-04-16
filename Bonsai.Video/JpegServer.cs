207e3db91ece9dc723e87cf3a003d2979a170ffd
glopesdev <glopesdev@users.noreply.github.com>
2020-04-16 18:04:43 +0100

    Simplify handling of HttpListener

--- Bonsai.Video/JpegServer.cs
+++ Bonsai.Video/JpegServer.cs
@@ -17,52 +17,37 @@ namespace Bonsai.Video
         [Description("The URL which will provide JPEG image files.")]
         public string SourceUrl { get; set; }
 
-        static IObservable<HttpListenerContext> GetContext(HttpListener listener)
+        internal static IObservable<byte[]> EncodeImage(IObservable<IplImage> source)
         {
-            return Observable
-                .Defer(() => listener.GetContextAsync().ToObservable())
-                .Repeat()
-                .Retry();
+            return source.Select(input =>
+            {
+                var data = CV.EncodeImage(".jpg", input);
+                var result = new byte[data.Cols];
+                using (var header = Mat.CreateMatHeader(result))
+                {
+                    CV.Copy(data, header);
+                }
+                return result;
+            });
         }
 
         public override IObservable<IplImage> Process(IObservable<IplImage> source)
         {
-            return source.Publish(ps =>
-            {
-                return ps.Merge(Observable.Using(
-                    () =>
-                    {
-                        var listener = new HttpListener();
-                        listener.Prefixes.Add(SourceUrl);
-                        listener.Start();
-                        return listener;
-                    },
-                    listener =>
-                    {
-                        var clients = GetContext(listener).Publish().RefCount();
-                        var jpgs = ps.Select(input =>
+            return source.Publish(ps => ps.Merge(Observable.Using(
+                () => new HttpListener(),
+                listener =>
+                {
+                    var frames = EncodeImage(ps).PublishReconnectable().RefCount();
+                    listener.Prefixes.Add(SourceUrl);
+                    listener.Start();
+                    return Observable
+                        .FromAsync(listener.GetContextAsync).Repeat().Retry()
+                        .SelectMany(context => frames.FirstAsync().Do(data =>
                         {
-                            var data = CV.EncodeImage(".jpg", input);
-                            var result = new byte[data.Cols];
-                            using (var header = Mat.CreateMatHeader(result))
-                            {
-                                CV.Copy(data, header);
-                            }
-                            return result;
-                        }).PublishReconnectable().RefCount();
-
-                        return clients.SelectMany(context =>
-                        {
-                            var response = context.Response;
-                            var stream = response.OutputStream;
-                            return jpgs.FirstAsync().Do(data =>
-                            {
-                                stream.Write(data, 0, data.Length);
-                                response.Close();
-                            });
-                        }).Retry().IgnoreElements().Select(x => default(IplImage));
-                    }));
-            });
+                            context.Response.OutputStream.Write(data, 0, data.Length);
+                            context.Response.Close();
+                        })).Retry().IgnoreElements().Select(x => default(IplImage));
+                })));
         }
     }
 }
