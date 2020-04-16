6e14c345447cf93cccf5b4b6a85f5d4706395765
glopesdev <glopesdev@users.noreply.github.com>
2020-04-16 21:36:54 +0100

    Remove unused using directives

--- Bonsai.Video/CameraPropertyCollection.cs
+++ Bonsai.Video/CameraPropertyCollection.cs
@@ -1,10 +1,4 @@
-﻿using AForge.Video.DirectShow;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 
 namespace Bonsai.Video
 {
